8d0863f705a6808fce8251fe36511fc7465513ca
Sherwin Mina <sherwinmina@yahoo.com>
2017-06-08 16:05:09 -0700

    styling porfolio page

--- src/components/header.js
+++ src/components/header.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react'
 import { Sidebar, Segment, Button, Menu, MenuItem, Image, Icon, Header } from 'semantic-ui-react'
 
 
-
 import About from './About';
+import Portfolio from './Portfolio';
 
 class SidebarLeftSlideOut extends Component {
   state = { visible: false }
@@ -18,21 +18,31 @@ class SidebarLeftSlideOut extends Component {
   render() {
     const { visible, active } = this.state
     return (
-      <div style={{paddingTop:"20px"}}>
-        <Menu tabular basic >
+      <div  color='blue' >
+        <Menu style={{color:'#F4F4F4', backgroundColor:'#1a1a1a', height: '100px'}} size='massive' tabular basic >
           
-          <MenuItem toggle  active={active} onClick={this.handleClick}>
-            <Icon name='sidebar'  size='large'/>
+          <MenuItem style={{color:'#F4F4F4', backgroundColor:'#eeeadd'}} toggle  active={active} onClick={this.handleClick}>
+            <Button style={{backgroundColor:'#c0b283'}} animated='vertical'>
+              <Button.Content icon_name="content" visible>
+                <Icon style={{color:'white'}} name='content' />
+              </Button.Content>
+              <Button.Content hidden>
+                <Icon style={{color:'white'}} name='down arrow' />
+              </Button.Content>
+            </Button>
           </MenuItem> 
 
+          <div>
+             <div className="first-name" >SHERWIN  </div ><div className="last-name"> Mina</div>
+             <div className="head-tag">(Web Developer)</div>
+          </div>
          
-          <h1>   Sherwin Mina</h1>
 
         </Menu>
 
 
-        <Sidebar.Pushable as={Segment}>
-          <Sidebar as={Menu} animation='slide out' width='thin' visible={visible} icon='labeled' vertical inverted>
+        <Sidebar.Pushable as={Segment} style={{marginTop:'-20px', backGroundColor:'#e2e1d0'}}>
+          <Sidebar as={Menu} animation='slide out' width='wide' visible={visible} icon='labeled' vertical inverted>
             <Menu.Item name='home'>
               <Icon name='home' />
               Home
@@ -42,20 +52,24 @@ class SidebarLeftSlideOut extends Component {
               <Icon name='user'  />
               About
             </Menu.Item>
+            <Menu.Item name='anchor'>
+              <Icon name='anchor' />
+              Portfolio
+            </Menu.Item>
             <Menu.Item name='mail'>
               <Icon name='mail' />
               Contact
             </Menu.Item>
           </Sidebar>
           
-          <Sidebar.Pusher>
-            <Segment basic>
+          <Sidebar.Pusher >
+            <Segment basic style={{ backgroundColor:'#282828', padding: '20px'}}>
               <Header as='h3'>About</Header>
               <About />  
             </Segment>
             <Segment basic>
-              <Header as='h3'>About</Header>
-              <About />
+              <Header as='h3'>Portfolio</Header>
+             <Portfolio />
               
             </Segment>
           </Sidebar.Pusher>
